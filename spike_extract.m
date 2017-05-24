ac4938f350f051e27339532365d236d90b9611f4
marianablfalcao <mariana.bl.falcao@gmail.com>
2017-05-24 19:44:44 +0200

    Add files via upload

--- spike_extract.m
+++ spike_extract.m
@@ -1,9 +1,9 @@
 
-function [spikes, spikesIndex]=spike_extract(neuralData,threshold,Fs,timeWindow)
+function [filteredSpikes, spikesIndex_final]=spike_extract(neuralData,threshold,Fs,timeWindow)
 
-% threshold: number of std as a threshold, 
-% Fs: sampling frequency in Hz, 
-% timeWindo: Time window for spike observation in seconds 
+% Thr = number of std as a threshold, Fs sampling in Hz, 
+% tw Time window for spikes observation in seconds, 
+% h_th is the artifact cutting threshold
 
 
 %% Thresholding
@@ -26,7 +26,7 @@ while(i<=(length(neuralData)-round(n/2)))
         k = k+1;
         spikes(k,:)=neuralData(round((i-n/2)):round((i+n/2-1)));
         spikesIndex(k)=i;
-        i=i+round(n); % with this we avoid that two spikes overlap.    
+        i=i+round(n); % with this we disable that two spikes overlap.    
     else
         i=i+1;
     end
@@ -36,8 +36,9 @@ end
 a=1;
 for(i=1:k)
     if(isempty(find(abs(spikes(i,:))>m+2*th)))
-        filteredSpikes(a,:)=spikes(i,:);
-        a=a+1;
+        filteredSpikes(a,:)=spikes(i,:);      
+        spikesIndex_final(a)=spikesIndex(i);
+    a=a+1;
     end
 end
 k=a-1;
